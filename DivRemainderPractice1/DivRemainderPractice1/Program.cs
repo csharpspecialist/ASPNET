c139c7d92d3652d1b835ce9a2de80508f6fb0bd2
csharpspecialist <netspec1@hotmail.com>
2017-06-27 21:41:34 -0400

    created 2 projects. 1 for computing a division remainder the other for a workbook project

---
+++ DivRemainderPractice1/DivRemainderPractice1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DivRemainderPractice1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int var1 = 167;
+            int var2 = 12;
+
+            int quotient, remainder;
+
+            quotient = var1 / var2;
+
+            remainder = var1 % var2;
+
+            WriteLine($"the quotient is {quotient} & the remainder is {remainder}");
+            ReadLine();
+        }
+    }
+}
---
+++ MultiplyUserInput/MultiplyUserInput/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace MultiplyUserInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WriteLine("Enter the first number to multiply");
+            string tempNumber1 = ReadLine();
+
+            int firstNumber = Convert.ToInt32(tempNumber1);
+
+            WriteLine("Enter the second number to multiply");
+            string tempNumber2 = ReadLine();
+            
+            int secondNumber = Convert.ToInt32(tempNumber2);
+            
+int total = firstNumber * secondNumber;
+
+WriteLine($"The first number {firstNumber} multiplied by {secondNumber} equals {total}");
+
+ReadLine();
+
+
+
+
+        }
+    }
+}
