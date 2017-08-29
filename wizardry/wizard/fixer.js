68a2de327b0e374090848a5b0c8dfdaa6196b571
csharpspecialist <netspec1@hotmail.com>
2017-08-29 02:20:18 -0400

    setting up jquery for Tuesday class

---
+++ wizardry/wizard/fixer.js
@@ -0,0 +1,40 @@
+﻿
+
+$(document).ready(function () {
+    $(".btn1").click(function () {
+        $("p").hide(2000);
+    });
+    $(".btn2").click(function () {
+        $("p").show(3000);
+    });
+
+
+
+});
+
+$(".flat").click(function () {
+    $("#div1").fadeIn();
+    $("#div2").fadeIn("slow");
+    $("#div3").fadeIn(3000);
+});
+
+$(".b2").click(function () {
+    $("#div1").fadeOut();
+    $("#div2").fadeOut("slow");
+    $("#div3").fadeOut(3000);
+});
+
+$("button").click(function () {
+    $("#p1").css("color", "red").slideUp(2000).slideDown(2000);
+});
+
+
+function feel() {
+
+    var store = document.getElementById('real').value;
+
+    document.getElementById('work').innerHTML = ("hello world...you typed  " + store);
+
+
+
+};
